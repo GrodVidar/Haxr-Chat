69f0012082b041a4e219580a3165414f800be6c9
Vidar Franzén <vexxart@gmail.com>
2020-01-05 13:10:01 +0100

    made the online feature link to a user DB that deletes users when leaving.

--- client.py
+++ client.py
@@ -2,7 +2,29 @@ from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 from tkinter import *
 import re
+import sqlite3
 from tkinter import font
+
+
+def update_online():
+    clients_list.delete(0, END)
+    clients_list.insert(END, "Online:")
+    for client in CLIENTS:
+        clients_list.insert(END, client)
+
+
+def get_online_users():
+    clients_connection = sqlite3.connect('online_users.db', check_same_thread=False)
+    clients_cursor = clients_connection.cursor()
+    clients_cursor.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='clients'")
+    if clients_cursor.fetchone()[0] == 1:
+        clients_cursor.execute("SELECT users FROM clients")
+        for row in clients_cursor.fetchall():
+            if row[0] not in CLIENTS:
+                print(f"adding {row[0]} to list")
+                CLIENTS.append(row[0])
+    clients_cursor.close()
+    clients_connection.close()
 
 
 def receive():
@@ -11,14 +33,19 @@ def receive():
             message = client_socket.recv(BUFFSIZE).decode('utf-8')
             if 'Announcer' in message:
                 if bool(re.search(r'\[(\w+)\]', message)):
-                    client = re.search(r'\[(\w+)\]', message)
-                    CLIENTS.append(client.group(1))
-                    for client in CLIENTS:
-                        clients_list.insert(END, client)
+                    get_online_users()
+                    update_online()
                 elif bool(re.search(r'\((\w+)\)', message)):
                     client = re.search(r'\((\w+)\)', message)
-                    clients_list.delete(CLIENTS.index(client.group(1))+1)  # +1 to not remove online: 
-                    CLIENTS.remove(client.group(1))
+                    print(f"delete {client.group(1)}")
+                    if str(client.group(1)) in CLIENTS:
+                        print("deleting")
+                        clients_list.delete(CLIENTS.index(client.group(1))+1)
+                        CLIENTS.remove(client.group(1))
+                        update_online()
+            elif 'quit()' in message:
+                get_online_users()
+                update_online()
             message_list.insert(END, message)
             message_list.see(END)
         except OSError:
@@ -30,6 +57,8 @@ def send(event=None):
     my_message.set("")  # Clears input field.
     client_socket.send(bytes(message, "utf8"))
     if message == "quit()":
+        # clients_cursor.close()
+        # clients_connection.close()
         client_socket.close()
         window.quit()
 
@@ -37,6 +66,7 @@ def send(event=None):
 def on_closing(event=None):
     my_message.set("quit()")
     send()
+
 
 HOST = input('Enter host: ')
 if not HOST:
@@ -63,6 +93,7 @@ except ValueError:
     print("setting default font-size")
     FONT_SIZE = 15
 
+
 CLIENTS = []
 window = Tk()
 window.title("Haxr-Chat")
@@ -72,6 +103,7 @@ top_frame.pack()
 bottom_frame = Frame(window, bg="black")
 bottom_frame.pack(side=BOTTOM)
 my_message = StringVar()
+get_online_users()
 
 clients_list = Listbox(top_frame, height=15, width=10, bg="black", fg="green", selectbackground="green", selectforeground="black", font=(FONT, FONT_SIZE))
 message_list = Listbox(top_frame, height=25, width=90, bg="black", fg="green", selectbackground="green", selectforeground="black", font=(FONT, FONT_SIZE))
@@ -87,6 +119,8 @@ send_button = Button(bottom_frame, text="Send", command=send)
 send_button.pack()
 
 clients_list.insert(END, "Online:")
+if len(CLIENTS) > 0:
+    update_online()
 
 
 window.protocol("WM_DELETE_WINDOW", on_closing)
--- server.py
+++ server.py
@@ -8,8 +8,11 @@ import sqlite3
 clients = {}
 addresses = {}
 
-connection = sqlite3.connect('chat.db', check_same_thread=False)
-my_cursor = connection.cursor()
+messages_connection = sqlite3.connect('chat.db', check_same_thread=False)
+messages_cursor = messages_connection.cursor()
+clients_connection = sqlite3.connect('online_users.db', check_same_thread=False)
+clients_cursor = clients_connection.cursor()
+
 
 HOST = ''
 PORT = input("enter port: ")
@@ -35,24 +38,38 @@ SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 SERVER.bind((HOST, PORT))
 
 
-def create_table(today):
-    my_cursor.execute(f"CREATE TABLE IF NOT EXISTS {today.strftime('%B_%d_%Y')}(date TEXT, user TEXT, message TEXT)")
+def create_table_messages(today):
+    messages_cursor.execute(f"CREATE TABLE IF NOT EXISTS {today.strftime('%B_%d_%Y')}(date TEXT, user TEXT, message TEXT)")
 
 
-def data_entry(today, date_format, user, message):
-    my_cursor.execute(f"INSERT INTO {today.strftime('%B_%d_%Y')}(date, user, message) VALUES (?, ?, ?)",
-                      (date_format, user, message))
-    connection.commit()
+def data_entry_messages(today, date_format, user, message):
+    messages_cursor.execute(f"INSERT INTO {today.strftime('%B_%d_%Y')}(date, user, message) VALUES (?, ?, ?)",
+                            (date_format, user, message))
+    messages_connection.commit()
 
+
+def create_table_clients():
+    clients_cursor.execute("CREATE TABLE IF NOT EXISTS clients (users TEXT)")
+
+
+def data_entry_clients(user):
+    if user != 'quit()':
+        clients_cursor.execute("INSERT INTO clients(users) VALUES(?)", (user,))
+        clients_connection.commit()
+
+
+def delete_client(name):
+    print(f"deleting: {name} from DB")
+    clients_cursor.execute("DELETE FROM clients WHERE users=(?)", (name,))
+    clients_connection.commit()
 
 
 def broadcast(message, prefix='Unknown: '):
-
     today = date.today()
-    create_table(today)
+    create_table_messages(today)
     # date_format = today.strftime("[%d %b-%y]")
     date_format = datetime.now().strftime('[%Y-%m-%d|%H:%M:%S]')
-    data_entry(today, date_format, prefix, message.decode('utf-8'))
+    data_entry_messages(today, date_format, prefix, message.decode('utf-8'))
     for s in clients:
         s.send(bytes(date_format + ' ' + prefix, 'utf-8')+message)
 
@@ -62,6 +79,8 @@ def handler(client):
     if len(name) == 1:
         client.send(bytes("name too short, setting name to Unknown", 'utf-8'))
         name = 'Unknown'
+    create_table_clients()
+    data_entry_clients(name)
     # TODO: for loopa igenom databasens table för att visa tidigare meddelanden för clienten ☺ så typ for message in db: client.send(bytes(f"{time} {name}: {message}"))
     client.send(bytes("welcome %s, to quit type quit()" % name, 'utf-8'))
     broadcast(bytes(f"[{name}] has joined the chat!", 'utf-8'), 'Announcer: ')
@@ -75,6 +94,7 @@ def handler(client):
             client.close()
             del clients[client]
             broadcast(bytes(f"({name}) has left the chat.", 'utf-8'), 'Announcer: ')
+            delete_client(name)
             break
 
 
@@ -94,6 +114,6 @@ if __name__ == "__main__":
     ACCEPT_THREAD = threading.Thread(target=accept_connections)
     ACCEPT_THREAD.start()
     ACCEPT_THREAD.join()
-    my_cursor.close()
-    connection.close()
+    messages_cursor.close()
+    messages_connection.close()
     SERVER.close()
