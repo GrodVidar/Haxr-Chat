61fcf5cccd72bf932ae68343ced74edb1f827d42
GrodVidar <vexxart@gmail.com>
2020-01-07 15:06:49 +0100

    send_temp fixed on cloud, removed all prints

--- server.py
+++ server.py
@@ -82,12 +82,10 @@ def send_temp():
     sent_this_minute = True
     with open("key.txt", 'r') as file:
         key = file.readline()
-        print(key)
         while True:
             if int(datetime.now().strftime('%M')) % 5 == 0 and not sent_this_minute:
                 resp = requests.get(f"https://api.openweathermap.org/data/2.5/weather?id=2673730&APPID={key}&units=metric")
                 my_json = json.loads(resp.text)
-                print(my_json)
                 broadcast(bytes(f"the weather in {my_json['name']} is {my_json['main']['temp']} C°", 'utf-8'), 'Weather-announcer: ')
                 sent_this_minute = True
             elif int(datetime.now().strftime('%M')) % 5 != 0 and sent_this_minute:
